c809b0861c9c41fa803f2dc1312d233b01508c84
Kurtis Jones <kurtis.jones@extendhealth.com>
2018-11-25 22:38:39 -0700

    Add project for command-line tool to convert romaji to hiragana/katakana

---
+++ convert-romaji/Program.cs
@@ -0,0 +1,24 @@
+﻿using battousai.jpParse;
+using System;
+
+namespace convert_romaji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var romaji = String.Join(" ", args);
+
+            if (String.IsNullOrWhiteSpace(romaji))
+                return;
+
+            var hiragana = NihonParser.ToHiragana(romaji);
+            var katakana = NihonParser.ToKatakana(romaji);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine(hiragana);
+            Console.WriteLine(katakana);
+        }
+    }
+}
